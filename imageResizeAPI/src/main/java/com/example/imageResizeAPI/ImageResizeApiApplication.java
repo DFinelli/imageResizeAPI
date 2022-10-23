a3e3f2153b2865583bc3c909a461af8a13384f52
DFinelli <danielfinelli@Daniels-MacBook-Pro.local>
2022-10-23 11:34:24 +0200

    spring boot app

---
+++ imageResizeAPI/src/main/java/com/example/imageResizeAPI/ImageResizeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageResizeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageResizeApiApplication.class, args);
+	}
+
+}
---
+++ imageResizeAPI/src/test/java/com/example/imageResizeAPI/ImageResizeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageResizeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
